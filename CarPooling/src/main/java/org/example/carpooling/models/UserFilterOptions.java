3de30f17e54ff91ca5f3d4da7f52c5eb14994587
Petar <peterkrandev@gmail.com>
2024-02-26 19:56:10 +0200

    User Dto classes

---
+++ CarPooling/src/main/java/org/example/carpooling/models/UserFilterOptions.java
@@ -0,0 +1,55 @@
+package org.example.carpooling.models;
+
+import java.util.Optional;
+
+public class UserFilterOptions {
+
+    private final Optional<String> firstName;
+    private final Optional<String> email;
+    private final Optional<String> username;
+    //TODO double check this since there is no post.id field in User to search from here
+//    private Optional<Integer> postId;
+    private final Optional<String> sortBy;
+    private final Optional<String> orderBy;
+
+    public UserFilterOptions() {
+        this.firstName = Optional.empty();
+        this.email = Optional.empty();
+        this.username = Optional.empty();
+        this.sortBy = Optional.empty();
+        this.orderBy = Optional.empty();
+    }
+
+    public UserFilterOptions(String firstName, String email,
+                             String username, String sortBy, String orderBy) {
+        this.firstName = Optional.ofNullable(firstName);
+        this.email = Optional.ofNullable(email);
+        this.username = Optional.ofNullable(username);
+//        this.postId = Optional.ofNullable(postId);
+        this.sortBy = Optional.ofNullable(sortBy);
+        this.orderBy = Optional.ofNullable(orderBy);
+    }
+
+
+
+    public Optional<String> getFirstName() {
+        return firstName;
+    }
+
+    public Optional<String> getEmail() {
+        return email;
+    }
+
+    public Optional<String> getUsername() {
+        return username;
+    }
+
+    public Optional<String> getSortBy() {
+        return sortBy;
+    }
+
+    public Optional<String> getOrderBy() {
+        return orderBy;
+    }
+
+}
---
+++ CarPooling/src/main/java/org/example/carpooling/models/dto/UserDto.java
@@ -0,0 +1,115 @@
+package org.example.carpooling.models.Dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+public class UserDto {
+    private static final String NAME_CAN_T_BE_EMPTY_MESSAGE = "Name can't be empty";
+    private static final String NUMBER_CAN_T_BE_EMPTY_MESSAGE = "Phone number can't be empty";
+    private static final String NUMBER_SHOULD_BE_10_DIGITS_MESSAGE = "Phone number should be 10 digits";
+    private static final String NAME_SHOULD_BE_BETWEEN_2_AND_20_SYMBOLS_MESSAGE = "Name should be between 2 and 20 symbols";
+    private static final String PASSWORD_REQUIREMENTS_MESSAGE = "Password should be minimum 8 symbols, contain capital " +
+            "letter and special symbol";
+
+
+
+
+    @NotNull(message = NAME_CAN_T_BE_EMPTY_MESSAGE)
+    @Size(min = 2, max = 20, message = NAME_SHOULD_BE_BETWEEN_2_AND_20_SYMBOLS_MESSAGE)
+    private String firstName;
+    @NotNull(message = NAME_CAN_T_BE_EMPTY_MESSAGE)
+    @Size(min = 2, max = 20, message = NAME_SHOULD_BE_BETWEEN_2_AND_20_SYMBOLS_MESSAGE)
+    private String lastName;
+
+    //ToDo double check how to use @Unique annotation
+    @NotNull(message = NAME_CAN_T_BE_EMPTY_MESSAGE)
+    private String email;
+
+    @NotNull(message = NUMBER_CAN_T_BE_EMPTY_MESSAGE)
+    @Size(min = 10, message = NUMBER_SHOULD_BE_10_DIGITS_MESSAGE)
+    private int phoneNumber;
+
+    //ToDo double check how to use @Unique annotation
+    @NotNull(message = NAME_CAN_T_BE_EMPTY_MESSAGE)
+    private String username;
+
+    @Size(min = 8, message = PASSWORD_REQUIREMENTS_MESSAGE)
+    @NotNull(message = NAME_CAN_T_BE_EMPTY_MESSAGE)
+    private String password;
+
+    public UserDto() {
+    }
+
+    public UserDto(String firstName, String lastName, String email, String username, int phoneNumber,String password) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmail(email);
+        setUsername(username);
+        setPhoneNumber(phoneNumber);
+        setPassword(password);
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDto userDto = (UserDto) o;
+        return Objects.equals(email, userDto.email) && Objects.equals(username, userDto.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, username);
+    }
+}
+
