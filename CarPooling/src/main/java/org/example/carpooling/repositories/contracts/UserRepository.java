28a33f2392378bca3fd6376a7f12dd1347571493
Petar <peterkrandev@gmail.com>
2024-02-26 19:56:55 +0200

    UserRepository interface and UserRepositoryImpl class

---
+++ CarPooling/src/main/java/org/example/carpooling/repositories/contracts/UserRepository.java
@@ -0,0 +1,36 @@
+package org.example.carpooling.repositories.contracts;
+
+import org.example.carpooling.models.User;
+import org.example.carpooling.models.UserFilterOptions;
+
+import java.util.List;
+
+public interface UserRepository {
+
+    List<User> getAllUsers(UserFilterOptions filterOptions);
+
+    User getByUserId(int UserId);
+
+    User getByFirstName(String firstName);
+
+    User getByEmail(String email);
+
+    User getByUsername(String username);
+
+    User create(User user);
+
+    User update(User user);
+
+    User delete(int id);
+
+    User makeUserAdmin(int id);
+
+    User unmakeUserAdmin(int id);
+
+    User blockUser(int id);
+
+    User unblockUser(int id);
+
+    long getUserCount();
+}
+
