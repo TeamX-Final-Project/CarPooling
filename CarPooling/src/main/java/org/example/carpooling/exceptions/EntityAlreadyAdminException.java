92a69ad12b3099953a439e55b308d4327c34c1d3
Petar <peterkrandev@gmail.com>
2024-03-05 03:26:10 +0200

    EntityAttributeAlreadySetException class

--- CarPooling/src/main/java/org/example/carpooling/exceptions/EntityAlreadyAdminException.java
+++
@@ -1,8 +0,0 @@
-package org.example.carpooling.exceptions;
-
-public class EntityAlreadyAdminException extends RuntimeException{
-    public EntityAlreadyAdminException(String type, String atribute, String value) {
-        super(String.format("%s with %s %s is already admin.", type, atribute, value));
-    }
-}
-
