5e78d79cd9654cac7a2fbe31a4b76af2b98bf8fb
Petar <peterkrandev@gmail.com>
2024-03-05 03:25:12 +0200

    hide api properties

--- CarPooling/src/main/java/org/example/carpooling/config/CloudinaryConfig.java
+++ CarPooling/src/main/java/org/example/carpooling/config/CloudinaryConfig.java
@@ -3,19 +3,27 @@ package org.example.carpooling.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloud.name}")
+    private String cloudName;
+
+    @Value("${cloud.api.key}")
+    private String apiKey;
+
+    @Value("${cloud.api.secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
-        Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dtakr4rhb",
-                "api_key", "549983154366957",
-                "api_secret", "buRSRG2MASUZmSJpzBJTtfyjNh8"));
-        return cloudinary;
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret));
     }
 }
