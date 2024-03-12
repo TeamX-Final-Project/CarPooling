5759adbb944b741114f5b445a11fe119e8f817d6
Petar <peterkrandev@gmail.com>
2024-03-12 16:40:49 +0200

    delete

--- CarPooling/src/main/java/org/example/carpooling/exceptions/FirstNameChangeProfileError.java
+++
@@ -1,7 +0,0 @@
-package org.example.carpooling.exceptions;
-
-public class FirstNameChangeProfileError extends RuntimeException{
-    public FirstNameChangeProfileError(String message) {
-        super(message);
-    }
-}
--- CarPooling/src/main/java/org/example/carpooling/exceptions/LastNameChangeProfileError.java
+++
@@ -1,7 +0,0 @@
-package org.example.carpooling.exceptions;
-
-public class LastNameChangeProfileError extends RuntimeException{
-    public LastNameChangeProfileError(String message) {
-        super(message);
-    }
-}
