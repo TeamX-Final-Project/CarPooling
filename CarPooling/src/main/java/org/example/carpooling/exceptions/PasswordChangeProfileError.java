92fdf5e99e6d2123180a16fc785f9f6cdd5cbf9c
Petar <peterkrandev@gmail.com>
2024-03-12 16:40:24 +0200

    delete

--- CarPooling/src/main/java/org/example/carpooling/exceptions/PasswordChangeProfileError.java
+++
@@ -1,7 +0,0 @@
-package org.example.carpooling.exceptions;
-
-public class PasswordChangeProfileError extends RuntimeException{
-    public PasswordChangeProfileError(String message) {
-        super(message);
-    }
-}
